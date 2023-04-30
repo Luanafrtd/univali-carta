9f7790af15d2847e913f22dcd75aab78118db2d4
Luana Furtado <programacaolufrtd@gmail.com>
2023-04-30 18:14:58 +1200

    project carta univali

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3>New Zealand, 30/04/2023</h3>
+
+      <body>
+       <p>Olá, como vai?</p>  
+      
+      <p>Eu estou ótima!  <br /> <br />
+         No momento estou morando em Auckland, New Zealand. <br />
+         Estou gostando muito e realmente é um lugar incrivel para se morar, estou estudando ingles em uma escola chamada ICL e fiz varias novas amizades. <br /> <br /></p>
+
+      <p>Em breve mandarei mais noticias...</p>
+
+      <h4>Att: Luana Furtado de Oliveira</h4>
+      </body>
       </header>
     </div>
+    
   );
 }
 
